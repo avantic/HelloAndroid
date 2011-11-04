be5bbc9cdc13eb6ff026e593c867db0f77c6b16f
Yeray Darias <ydarias@gmail.com>
2011-11-04 19:57:07 +0000

    Added application lifecycle toasts

--- src/net/avantic/HelloAndroidActivity.java
+++ src/net/avantic/HelloAndroidActivity.java
@@ -1,13 +1,57 @@
 package net.avantic;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.Toast;
 
 public class HelloAndroidActivity extends Activity {
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        showToast("onCreate call");
     }
+    
+    @Override
+    protected void onStart() {
+    	super.onStart();
+    	showToast("onStart call");
+    }
+    
+    @Override
+    protected void onResume() {
+    	super.onResume();
+    	showToast("onResume call");
+    }
+    
+    @Override
+    protected void onPause() {
+    	super.onPause();
+    	showToast("onPause call");
+    }
+    
+    @Override
+    protected void onStop() {
+    	super.onStop();
+    	showToast("onStop call");
+    }
+    
+    @Override
+    protected void onDestroy() {
+    	super.onDestroy();
+    	showToast("onDestroy call");
+    }
+    
+    private void showToast(String msg) {
+		Context context = getApplicationContext();
+		int duration = Toast.LENGTH_SHORT;
+		Toast toast = Toast.makeText(context, msg, duration);
+		toast.setGravity(Gravity.CENTER, 10, 10);
+		toast.show();
+	}
+    
 }
