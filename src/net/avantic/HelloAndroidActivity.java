450f29d89bf2b8eefe744ec1667cccc5536ab34d
Yeray Darias <ydarias@gmail.com>
2011-11-04 22:11:38 +0000

    Added an options menu to hide messages

--- src/net/avantic/HelloAndroidActivity.java
+++ src/net/avantic/HelloAndroidActivity.java
@@ -1,57 +1,80 @@
 package net.avantic;
 
 import android.app.Activity;
-import android.content.Context;
 import android.os.Bundle;
-import android.view.Gravity;
-import android.widget.Toast;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class HelloAndroidActivity extends Activity {
+	
+	UserMessages messages;
 	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        showToast("onCreate call");
+        
+        messages = new UserMessages(getApplicationContext());
+        
+        messages.show("onCreate call");
     }
     
     @Override
     protected void onStart() {
     	super.onStart();
-    	showToast("onStart call");
+    	messages.show("onStart call");
     }
     
     @Override
     protected void onResume() {
     	super.onResume();
-    	showToast("onResume call");
+    	messages.show("onResume call");
     }
     
     @Override
     protected void onPause() {
     	super.onPause();
-    	showToast("onPause call");
+    	messages.show("onPause call");
     }
     
     @Override
     protected void onStop() {
     	super.onStop();
-    	showToast("onStop call");
+    	messages.show("onStop call");
     }
     
     @Override
     protected void onDestroy() {
     	super.onDestroy();
-    	showToast("onDestroy call");
+    	messages.show("onDestroy call");
     }
     
-    private void showToast(String msg) {
-		Context context = getApplicationContext();
-		int duration = Toast.LENGTH_SHORT;
-		Toast toast = Toast.makeText(context, msg, duration);
-		toast.setGravity(Gravity.CENTER, 10, 10);
-		toast.show();
-	}
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+    	getMenuInflater().inflate(R.menu.options, menu);
+    	return super.onCreateOptionsMenu(menu);
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+    	switch (item.getItemId()) {
+			case R.id.messagesItem:
+				if (item.isChecked()) {
+					messages.showAlways("Now I will not show messages anymore");
+					messages.disable();
+					item.setChecked(false);
+				} else {
+					messages.showAlways("Now I'll show messages");
+					messages.enable();
+					item.setChecked(true);
+				}
+				break;
+			default:
+				messages.show("I shouldn't be here, something is wrong");
+				break;
+		}
+    	return super.onOptionsItemSelected(item);
+    }
     
 }
