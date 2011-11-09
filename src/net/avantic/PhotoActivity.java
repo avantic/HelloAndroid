ebba3b49a1ec6e7e8b74864477fd131711d404f9
Yeray Darias <ydarias@gmail.com>
2011-11-09 12:26:35 +0000

    The camera feature can save photos in the sd card

--- src/net/avantic/PhotoActivity.java
+++ src/net/avantic/PhotoActivity.java
@@ -1,10 +1,18 @@
 package net.avantic;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -13,7 +21,7 @@ import android.widget.ImageView;
 public class PhotoActivity extends Activity implements OnClickListener {
 	
 	private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -32,11 +40,44 @@ public class PhotoActivity extends Activity implements OnClickListener {
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+			// show preview image
 			Bitmap bitmap = (Bitmap) data.getExtras().get("data");
-			ImageView image = (ImageView)findViewById(R.id.showPhotoView);
-			image.setImageBitmap(bitmap);
+			ImageView imagePreview = (ImageView)findViewById(R.id.showPhotoView);
+			imagePreview.setImageBitmap(bitmap);
+			
+			// save image in the default app directory
+			try {
+				File imageFile = getOutputMediaFile();
+				FileOutputStream os = new FileOutputStream(imageFile);
+				bitmap.compress(Bitmap.CompressFormat.JPEG, 100, os);
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}
 		}
 		super.onActivityResult(requestCode, resultCode, data);
+	}
+	
+	/** Create a File for saving an image or video */
+	private static File getOutputMediaFile(){
+	    // To be safe, you should check that the SDCard is mounted
+	    // using Environment.getExternalStorageState() before doing this.
+
+		File storagePublicDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+	    File mediaStorageDir = new File(storagePublicDirectory, "MyCameraApp");
+	    // This location works best if you want the created images to be shared
+	    // between applications and persist after your app has been uninstalled.
+
+	    // Create the storage directory if it does not exist
+	    if (! mediaStorageDir.exists()){
+	        if (! mediaStorageDir.mkdirs()){
+	            Log.d("MyCameraApp", "failed to create directory");
+	            return null;
+	        }
+	    }
+
+	    // Create a media file name
+	    String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+	    return new File(mediaStorageDir.getPath() + File.separator + "IMG_"+ timeStamp + ".jpg");
 	}
 	
 }
